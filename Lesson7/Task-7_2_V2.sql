3a51960971c0dfe9ddf14722baabbb8f8d56bf3d
ekspertas <74507149+ekspertas@users.noreply.github.com>
2021-04-03 12:01:48 +0300

    Update Task-7_2_V2.sql

--- Lesson7/Task-7_2_V2.sql
+++ Lesson7/Task-7_2_V2.sql
@@ -9,7 +9,7 @@
 SELECT	p2.name AS 'Название товара',
 		c2.name AS 'Раздел'
 	FROM products p2  
-	LEFT JOIN catalogs c2 		-- т.к. первой (правой) таблицей идет products
+	LEFT JOIN catalogs c2 		-- т.к. первой (левой) таблицей идет products
 	ON p2.catalog_id = c2.id;	/* объединяем таблицы по id в каталоге. 
 	Если в таблице products нет товаров с catalog_id равному id из таблицы catalogs,
 	то товар будет выведен и значение "раздел" будет отображно как NULL*/	
